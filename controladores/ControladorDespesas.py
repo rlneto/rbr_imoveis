822a4917ef615283d42b8c3b7a70fafd391b3d3c
Betina Corazza Ferrao <betina.ferrao20002@gmail.com>
2024-05-18 12:10:24 -0300

    despesas

--- controladores/ControladorDespesas.py
+++ controladores/ControladorDespesas.py
@@ -8,10 +8,35 @@
 # 
 #######################################################
 from limites.TelaDespesas import TelaDespesas
+from limites.TelaDespesas import TelaDespesas
+# from limites.TelaPopup import TelaPopup 
 from DAOs.DAODespesa import DAODespesa
 from controladores.ControladorGeraIdDespesa import ControladorGeraIdDespesa
 
 class ControladorDespesas:
+    C_DESPESAS = "C_DESPESAS"
+    R_DESPESAS = "R_DESPESAS"
+    D_DESPESAS = "D_DESPESAS"
+    PROXIMO = "PROXIMO"
+    VOLTAR = "VOLTAR"
+
+    def __init__(self):
+        self.__dao = DAODespesa("despesas.pkl")
+        self.__tela = TelaDespesas()
+
+    def abrir_menu(self):
+        while True:
+            match self.__tela.abrir_menu():
+                case self.C_DESPESAS:
+                    self.cadastrar_despesa()
+                case self.R_DESPESAS:
+                    self.listar_despesas()
+                case self.D_DESPESAS:
+                    self.exclui_despesa()
+                case self.VOLTAR:
+                    return
+                case None:
+                    return
 
 
     def cadastrar_despesa(self):
